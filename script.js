129d62197f6713533cbb2062723ca09889bd1648
26adarsh <mishraadarsh2609@gmail.com>
2023-12-16 11:58:18 +0530

    1st commit

---
+++ script.js
@@ -0,0 +1,21 @@
+// script.js
+
+const chatForm = document.getElementById('chat-form');
+const chatMessages = document.querySelector('.chat-messages');
+
+chatForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const userInput = document.getElementById('user-input').value;
+    displayMessage(userInput, 'user');
+    // Send user input to the server and get the response
+    // Display the response in the chat messages
+    document.getElementById('user-input').value = '';
+});
+
+function displayMessage(message, sender) {
+    const div = document.createElement('div');
+    div.classList.add('message');
+    div.classList.add(sender);
+    div.innerText = message;
+    chatMessages.appendChild(div);
+}
